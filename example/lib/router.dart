4d3bc256bbe95ab1bc1b7151ed859e9681cbc3da
Dimitri Dessus <d.dessus@apparence.io>
2020-08-26 11:20:40 +0200

    🗑 Remove unused code

--- example/lib/router.dart
+++ example/lib/router.dart
@@ -2,13 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:pal_example/ui/pages/home/home_page.dart';
 import 'package:pal_example/ui/pages/route1/route1_page.dart';
 import 'package:pal_example/ui/pages/route2/route2_page.dart';
-import 'package:palplugin/palplugin.dart';
 
 Route<dynamic> route(RouteSettings settings) {
-  // Notify Pal plugin when a new page is pushed
-  // FIXME: Need to handle Pop
-  PalController.instance.routeName.value = settings.name;
-
   switch (settings.name) {
     case '/':
       return MaterialPageRoute(

a7bbeb52e8c16605eb28d65eddf43d2f19551c20
Kirill <knikolnikov@gmail.com>
2020-09-22 11:29:32 +0200

    :sparkles: Created progress Widget

---
+++ lib/src/ui/shared/widgets/progress_widget/progress_bar.dart
@@ -0,0 +1,114 @@
+
+import 'package:flutter/material.dart';
+import 'package:palplugin/src/theme.dart';
+
+import 'pulsing_circle.dart';
+
+class ProgressWidget extends StatefulWidget {
+  final double nbSteps;
+  final double step;
+
+  const ProgressWidget({Key key, this.nbSteps, this.step}) : super(key: key);
+
+  @override
+  _ProgressWidgetState createState() => _ProgressWidgetState();
+}
+
+class _ProgressWidgetState extends State<ProgressWidget>
+    with SingleTickerProviderStateMixin {
+  // CORE ATTRIBUTES
+  AnimationController controller;
+  Animation animation;
+
+  // STEPS VARIABLES
+  double _nbSteps;
+
+  @override
+  void initState() {
+    // SETUP
+    // INITIALIZING STEPS : Bringing them to a smaller scale from 0 to 1*
+    this._nbSteps = 1/(widget.nbSteps-1);
+
+    // CONTROLLER AND ANIMATION INIT
+    this.controller = AnimationController(vsync: this, duration: Duration(seconds: 1));
+    // ANIMATES THE PROGRESS BAR FROM [STEP-1] TO [STEP]
+    this.animation = Tween<double>(begin: 0, end: this._nbSteps*widget.step).animate(this.controller);
+    // ANIMATES
+    this.controller.forward();
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(ProgressWidget oldWidget) {
+    // ONLY TRIGGERS WHEN ATTRIBUTES ARE CHANGED
+    if (oldWidget.step != widget.step) {
+      this.controller.reset();
+      // CREATES NEW ANIMATION FROM NEW VALUES
+      this.animation = Tween<double>(begin: this._nbSteps*(widget.step-1), end: this._nbSteps*widget.step).animate(this.controller);
+      this.controller.forward();
+      super.didUpdateWidget(oldWidget);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 25,
+
+        // PLACING PULSING CIRCLES ON-TOP THE PROGRESS BAR
+        child: Stack(
+
+        children: [
+          // PROGRESS BAR
+          Align(
+            alignment: Alignment.center,
+
+            child: FractionallySizedBox(
+              widthFactor: 0.9,
+
+              child: AnimatedBuilder(
+                animation: this.controller,
+
+                builder: (context, child) => 
+                DecoratedBox(
+                  decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    //* ANIMATION MOVES THE LEFT GRADIENT COLOR
+                    stops: [this.animation.value, 0],
+                    // IN THE ORDER BELOW : DARK / GREY
+                    colors: [PalTheme.of(context).colors.dark,Color(0xFFC1BFD6)]),
+                  ),
+                  child: child),
+
+                // SETUPS THE BAR HEIGHT
+                child: Container(
+                  height: 4,
+                ),
+              ),
+            ),
+          ),
+
+          // PUSLING CIRCLES
+          Align(
+            alignment: Alignment.center,
+
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+              // CREATES AS MANY PUSLING CIRCLES AS THERE OF STEPS
+              children: [
+              for (var i = 0; i < widget.nbSteps; i++)
+                // CONTAINER NEEDED TO CREATE AN ALLOCATED SPACE FOR CIRCLE TU PULSE WITHOUR MOVING OTHERS
+                Container(
+                  width: 25,
+
+                  child: PulsingCircle(active: i==widget.step,done: i<widget.step),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
---
+++ lib/src/ui/shared/widgets/progress_widget/pulsing_circle.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:palplugin/src/theme.dart';
+
+class PulsingCircle extends StatefulWidget {
+  final bool active;
+  final bool done;
+
+  const PulsingCircle({Key key, this.active, this.done}) : super(key: key);
+
+  @override
+  _PulsingCircleState createState() => _PulsingCircleState();
+}
+
+class _PulsingCircleState extends State<PulsingCircle> with SingleTickerProviderStateMixin{
+  // CORE ATTRIBUTES
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    // INITIALIZING PULSING ANIMATION
+    this.controller = AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    // THE ACTIVE CIRCLE IS PULSATING BEETWEEN 8-12 OF RADIUS (16-24)
+    this.animation = Tween<double>(begin:8,end:12).animate(this.controller);
+    // REPEATS INFINIT
+    this.controller.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: this.controller,
+
+        builder: (context, child) =>
+         CircleAvatar(
+          //  COLOR BASED ON IF THE CIRCLE IS DONE / ACTIVE / NONE
+          // IN THE ORDER BELOW : DARK / CYAN / GREY
+          backgroundColor: widget.done ? PalTheme.of(context).colors.dark  : widget.active ? PalTheme.of(context).colors.color3 : Color(0xFFC1BFD6) ,
+          // IF THE CIRCLE IS ACTIVE : ANIMATED / IF NOT : STATIC
+          radius: widget.active ? this.animation.value : 8,
+        ),
+      );
+  }
+}

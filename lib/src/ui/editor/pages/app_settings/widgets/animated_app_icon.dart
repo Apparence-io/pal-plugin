16e257a2848039b29dacbc5d8961fc1e42648275
Dimitri Dessus <d.dessus@apparence.io>
2020-10-28 15:12:55 +0100

    🤡 Disable refresh button

--- lib/src/ui/editor/pages/app_settings/widgets/animated_app_icon.dart
+++ lib/src/ui/editor/pages/app_settings/widgets/animated_app_icon.dart
@@ -46,25 +46,30 @@ class AnimatedAppIcon extends AnimatedWidget {
         child: Stack(
           children: [
             ClipOval(child: !testMode ? AppIconImage() : Container()),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: CircleIconButton(
-                key: ValueKey(
-                    'pal_AppSettingsPage_AnimatedAppIcon_RefreshButton'),
-                isLoading: isSendingAppIcon,
-                icon: Icon(
-                  Icons.refresh,
-                  color: PalTheme.of(context).colors.light,
+            IgnorePointer(
+              child: Opacity(
+                opacity: 0.5,
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: CircleIconButton(
+                    key: ValueKey(
+                        'pal_AppSettingsPage_AnimatedAppIcon_RefreshButton'),
+                    isLoading: isSendingAppIcon,
+                    icon: Icon(
+                      Icons.refresh,
+                      color: PalTheme.of(context).colors.light,
+                    ),
+                    backgroundColor: PalTheme.of(context).colors.dark,
+                    onTapCallback: !isSendingAppIcon
+                        ? () {
+                            if (onTap != null) {
+                              HapticFeedback.selectionClick();
+                              onTap();
+                            }
+                          }
+                        : null,
+                  ),
                 ),
-                backgroundColor: PalTheme.of(context).colors.dark,
-                onTapCallback: !isSendingAppIcon
-                    ? () {
-                        if (onTap != null) {
-                          HapticFeedback.selectionClick();
-                          onTap();
-                        }
-                      }
-                    : null,
               ),
             )
           ],

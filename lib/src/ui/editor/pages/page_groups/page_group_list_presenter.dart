b1638d80f378d9dc0bc001f94e2c40f54385d5a1
Kiirii <k.nikolnikov@apparence.io>
2021-03-01 09:23:06 +0100

    Added [pageId] for 'serverGroupFetch' on GroupListPage

--- lib/src/ui/editor/pages/page_groups/page_group_list_presenter.dart
+++ lib/src/ui/editor/pages/page_groups/page_group_list_presenter.dart
@@ -3,23 +3,26 @@ import 'package:mvvm_builder/mvvm_builder.dart';
 import 'package:pal/pal.dart';
 import 'package:pal/src/injectors/editor_app/editor_app_injector.dart';
 import 'package:pal/src/services/editor/groups/group_service.dart';
+import 'package:pal/src/services/editor/page/page_editor_service.dart';
 
 import 'page_group_list.dart';
 import 'page_group_list_model.dart';
 
 /// [PageGroupsListPresenter]
-/// business logic for page group list 
-class PageGroupsListPresenter extends Presenter<PageGroupsListViewModel, PageGroupsListView> {
-
+/// business logic for page group list
+class PageGroupsListPresenter
+    extends Presenter<PageGroupsListViewModel, PageGroupsListView> {
   final EditorHelperGroupService helperGroupService;
   final PalNavigatorObserver navigatorObserver;
-  
+  final PageEditorService pageService;
+
   PageGroupsListPresenter({
     EditorInjector editorInjector,
     PageGroupsListView viewInterface,
     this.navigatorObserver,
-  }) : this.helperGroupService = editorInjector.helperGroupService, 
-    super(PageGroupsListViewModel(), viewInterface);
+  })  : this.helperGroupService = editorInjector.helperGroupService,
+        this.pageService = editorInjector.pageEditorService,
+        super(PageGroupsListViewModel(), viewInterface);
 
   @override
   void onInit() async {
@@ -29,32 +32,32 @@ class PageGroupsListPresenter extends Presenter<PageGroupsListViewModel, PageGro
     RouteSettings route = await navigatorObserver.routeSettings.first;
     // TODO show error if route name is empty
     // TODO show current page route path
-    helperGroupService.getPageGroups(route.name)
-      .catchError((err) {
+    this.pageService.getOrCreatePageId(route.name).catchError((err) {
+      viewModel.errorMessage = "Server error while loading data...";
+      viewModel.isLoading = false;
+      refreshView();
+    }).then((id) {
+      helperGroupService.getPageGroups(id).catchError((err) {
         viewModel.errorMessage = "Server error while loading data...";
         viewModel.isLoading = false;
         refreshView();
-      })
-      .then((groupsEntities) {
-        if(viewModel.errorMessage != null) 
-          return;
-        if(viewModel.groups.isNotEmpty)
-          viewModel.groups.clear();
+      }).then((groupsEntities) {
+        if (viewModel.errorMessage != null) return;
+        if (viewModel.groups.isNotEmpty) viewModel.groups.clear();
         groupsEntities.forEach((element) {
-          if(!viewModel.groups.containsKey(element.triggerType)) {
+          if (!viewModel.groups.containsKey(element.triggerType)) {
             viewModel.groups.putIfAbsent(element.triggerType, () => List());
           }
-          viewModel.groups[element.triggerType]
-            .add(GroupItemViewModel(
-              element.name, 
-              _formatDate(element.creationDate), 
+          viewModel.groups[element.triggerType].add(GroupItemViewModel(
+              element.name,
+              _formatDate(element.creationDate),
               _formatVersion(element.minVersion, element.maxVersion),
-              element.id
-            ));
+              element.id));
         });
         viewModel.isLoading = false;
         refreshView();
       });
+    });
   }
 
   void onClickClose() => viewInterface.closePage();
@@ -65,8 +68,9 @@ class PageGroupsListPresenter extends Presenter<PageGroupsListViewModel, PageGro
     viewInterface.navigateCreateHelper(route.name);
   }
 
-  String _formatDate(DateTime date) => "Created on ${date.day}/${date.month}/${date.year}"; 
+  String _formatDate(DateTime date) =>
+      "Created on ${date.day}/${date.month}/${date.year}";
 
-  String _formatVersion(String minVersion, String maxVersion) => "$minVersion - ${maxVersion ?? 'last'}"; 
-
+  String _formatVersion(String minVersion, String maxVersion) =>
+      "$minVersion - ${maxVersion ?? 'last'}";
 }
